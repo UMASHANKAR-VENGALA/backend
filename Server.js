e39a2f8bf1fe0ff04a3b7d72a4650b7dfca73bc8
UMASHANKAR-VENGALA <2200031870@kluniversity.in>
2024-04-28 17:54:23 +0530

    Final Backend

--- Server.js
+++ Server.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+require('dotenv').config();
 
-const dburl = "mongodb://localhost:27017/sdpproject32"
+// const dburl = "mongodb://localhost:27017/sdpproject32"
+
+const dburl = process.env.mongodburl
+
+
 mongoose.connect(dburl).then(() => {
     console.log("db connected successfully")
 }).catch((err) => {
     console.log(err.message)
 });
   
+
+// MongoDB Atlas Connection
+// const dburl = "mongodb+srv://klu:klu@cluster0.3q0v9x3.mongodb.net/demoproject32?retryWrites=true&w=majority"
+// mongoose.connect(dburl).then(() => {
+//     console.log("Connected to MongoDB Atlas Successfully")
+// }).catch((err) => {
+//     console.log(err.message)
+// });
 
 
 const app = express()
